5d96b85f3269c38171bd3b13ebf10150ea68d703
Jurema da c Fonseca <juremaguerreira01a@gmail.com>
2024-07-12 11:53:42 -0300

    teste

---
+++ Mycalcs.py
@@ -0,0 +1,15 @@
+#Demonstraçãodo uso de operadores!
+X = int(input("digitr um número: "))
+Y = int(input("digite quatro números: "))
+
+SOMA = X = Y
+print("O resultado da Operação é:", SOMA)
+
+SUBTRACAO = X - Y
+print("O resultado dad subtração é:", SUBTRACAO)
+
+MULTIPLICACAO = X * Y
+print("O resultado da multiplicação é", MULTIPLICACAO)
+
+DIVISAO = X / Y
+print("O resultado da divisão é", DIVISAO)
